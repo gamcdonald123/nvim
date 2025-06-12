3d94f0e379c0b0758a068e2b70a1542bf4addf90
gamcdonald123 <58171259+gamcdonald123@users.noreply.github.com>
2025-06-12 13:32:10 +0100

    refreshed keymaps

--- lua/guy/core/keymaps.lua
+++ lua/guy/core/keymaps.lua
@@ -1,7 +1,41 @@
-vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope find_files<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>r", ":Telescope treesitter<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", ";", ":", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>t", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>c", ":NvimTreeCollapse<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>b", ":Telescope buffers<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>h", ":noh<CR>", { noremap = true, silent = true })
+-- Telescope keymaps
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true, desc = "Telescope: Find files" })
+vim.keymap.set("n", "<leader>r", "<cmd>Telescope treesitter<CR>", { noremap = true, silent = true, desc = "Telescope: Treesitter symbols" })
+vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true, desc = "Telescope: Buffers" })
+
+-- NvimTree keymaps
+vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle NvimTree" })
+vim.keymap.set("n", "<leader>c", "<cmd>NvimTreeCollapse<CR>", { noremap = true, silent = true, desc = "Collapse NvimTree" })
+
+-- Clear search highlights
+vim.keymap.set("n", "<leader>h", "<cmd>noh<CR>", { noremap = true, silent = true, desc = "Clear search highlights" })
+
+-- Command mode shortcut
+vim.keymap.set("n", ";", ":", { noremap = true, desc = "Command mode" })
+
+-- Lspsaga-powered LSP navigation
+vim.keymap.set("n", "gd", function()
+  local params = vim.lsp.util.make_position_params()
+  vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result)
+    if err then
+      vim.notify("LSP error: " .. err.message, vim.log.levels.ERROR)
+      return
+    end
+    if not result or vim.tbl_isempty(result) then
+      vim.notify("No definition found", vim.log.levels.WARN)
+      return
+    end
+
+    if vim.tbl_islist(result) and #result == 1 then
+      vim.lsp.util.jump_to_location(result[1], "utf-8")
+    else
+      -- fallback to Telescope if multiple locations
+      require("telescope.builtin").lsp_definitions()
+    end
+  end)
+end, { desc = "Smart Go to Definition" })
+vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { noremap = true, silent = true, desc = "Lspsaga: Peek definition" })
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { noremap = true, silent = true, desc = "Lspsaga: Hover doc" })
+
+-- Jump back in location history
+vim.keymap.set("n", "<BS>", "<C-o>", { noremap = true, silent = true, desc = "Jump back in definition history" })
